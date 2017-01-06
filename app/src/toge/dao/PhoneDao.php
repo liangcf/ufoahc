dff0032ee83bb852bdddf43254a7d070b4799715
AF <lcfuzj@163.com>
2017-01-06 21:56:00 +0800

    lizi

--- app/src/toge/dao/PhoneDao.php
+++ app/src/toge/dao/PhoneDao.php
@@ -12,15 +12,16 @@ class PhoneDao extends DbConnect
     function __construct(){
         //TODO 默认的 类名必须与数据库表名相同切后有Dao 例如: 表名'users',类名'UsersDao'
         //////////////////////开始//////////////////////////
-        $tmp=explode('\\',__CLASS__);
+        /*$tmp=explode('\\',__CLASS__);
         $count=count($tmp);
         $_name=$tmp[$count-1];
-        parent::__construct(substr(strtolower($_name),0,-3));
+        parent::__construct(substr(strtolower($_name),0,-3));*/
         ///////////////////////结束/////////////////////////
 
         //TODO 直接继承 修改构造函数参数
         /********************开始***********************/
-        //parent::__construct('users','id');
+        parent::__construct('phone');
+//        parent::__construct('phone','id');
         /********************结束***********************/
     }
 }
